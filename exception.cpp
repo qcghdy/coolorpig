ef260ac3e3a571c872e6a3ccbfd9168b80be9e24
朱飞虹 <feihong.zhu.qiao@gmail.com>
2014-07-24 13:03:41 +0800

    继承链中的异常
    心得：
    demo3
    应用程序底层函数，可以尽可能的抛出比较详细类型的异常，如RelayExp；但程序最外层一般也需要尽可能详细的获取该异常；但中间层，异常某些信息，如错误码、错误描述等需要变更、增加更详细的描述时，我们可以在中间层用继承链中的父类去接收该异常,并对异常信息进行修改，并用throw抛出。注意，这里要用throw，而不是throw obj；因为单throw时会抛异常发生时的真实类型，但throw obj则只能抛obj的类型。

--- exception.cpp
+++ exception.cpp
@@ -12,19 +12,19 @@ public:
 	{
 	}
 	
-	Exp& set_error(string& strError)
+	virtual Exp& set_error(string& strError)
 	{
 		m_error = strError;
 		return *this;
 	}
 
-	Exp& set_error(const char * szError )
+	virtual Exp& set_error(const char * szError )
 	{
 		m_error = szError ? szError : ""; 
 		return *this;
 	}
 
-	Exp& set_errno(const int& errcode)
+	virtual Exp& set_errno(const int& errcode)
 	{
 		m_errno = errcode;
 		return *this;
@@ -45,9 +45,23 @@ public:
 		os << "errno=" << m_errno << "&error=" << m_error;
 		return os;
 	}
+	
+	virtual ~Exp(){}
 private:
 	int m_errno;
 	string m_error;
+};
+
+class RelayExp : public Exp
+{
+public:
+	RelayExp(int errcode = 0, string strErrorInfo = "")
+	:Exp(errcode, strErrorInfo)
+	{
+
+	}
+	
+	virtual ~RelayExp(){} 
 };
 
 int main()
@@ -55,7 +69,105 @@ int main()
 	std::stringstream oss;
 	Exp().set_error("row not found").set_errno(10).dump(oss);
 	string info = oss.str(); 
-	std::cout<< info << std::endl;
+	std::cout << info << std::endl;
+
+	oss.str("");	
 	
+	RelayExp().set_error("connection failed...").set_errno(20).dump(oss);
+	std::cout << oss.str() << std::endl;	
+	
+	std::cout << "----------------demo1--------------------" << std::endl;	
+	try
+	{
+		throw RelayExp().set_error("connection failed...").set_errno(20);
+	}
+	catch(const RelayExp& exp)
+	{
+		std::cout << "Relay Exption:" << exp.get_error() << std::endl;
+		std::cout << typeid(exp).name() << std::endl;
+	}
+	catch(const Exp& exp)
+	{
+		std::cout << "Exption:" << exp.get_error() << std::endl;
+		std::cout << typeid(exp).name() << std::endl;
+	}
+
+	std::cout << "----------------demo2--------------------" << std::endl;	
+	try
+	{
+		RelayExp f;
+		f.set_error("connection failed...").set_errno(20);
+		throw f;
+	}
+	catch(const Exp& exp)
+	{
+		std::cout << "Exption:" << exp.get_error() << std::endl;
+		std::cout << typeid(exp).name() << std::endl;
+	}	
+	catch(const RelayExp& exp)
+	{
+		std::cout << "Relay Exption:" << exp.get_error() << std::endl;
+		std::cout << typeid(exp).name() << std::endl;
+	}
+
+	std::cout << "----------------demo3--------------------" << std::endl;	
+	try
+	{
+		RelayExp f;
+		f.set_error("org relay exception -- connection failed...").set_errno(201);
+		throw f;
+	}
+	catch(Exp& exp)
+	{
+		std::cout << "Exption:" << exp.get_error() << std::endl;
+		std::cout << typeid(exp).name() << std::endl;
+		try
+		{
+#if 0
+			//case 1,throw Exp obj
+			//throw exp; 
+#endif
+
+#if 1
+			//case 2, modify obj, then throw real type,this case is RelayExp
+			exp.set_error("org relay exception has been caught by Exp obj, Exp obj modify error info -- connection server failed...").set_errno(2021);
+			throw;       
+#endif
+		}
+		catch(const RelayExp& exp)
+		{
+			std::cout << "Relay Exption:" << exp.get_error() << std::endl;
+			std::cout << typeid(exp).name() << std::endl;
+		}
+		catch(const Exp& exp)
+		{
+			std::cout << "Exption:" << exp.get_error() << std::endl;
+			std::cout << typeid(exp).name() << std::endl;
+		}
+		catch(...)
+		{
+			std::cout << "unKown Exption:"  << std::endl;
+		}
+	}	
+
+
 	return 0;
 }
+
+/* *tese result*
+
+errno=10&error=row not found
+errno=20&error=connection failed...
+----------------demo1--------------------
+Exption:connection failed...
+3Exp
+----------------demo2--------------------
+Exption:connection failed...
+8RelayExp
+----------------demo3--------------------
+Exption:org relay exception -- connection failed...
+8RelayExp
+Relay Exption:org relay exception has been caught by Exp obj, Exp obj modify error info -- connection server failed...
+8RelayExp
+
+*/
